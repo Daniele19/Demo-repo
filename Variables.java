f2da5959891e08edb0fc9ec3c267e261552d8839
Daniele19 <danielehaenel19@gmail.com>
2021-10-27 01:26:20 +0200

    chapter on Variables in java,
    homework on Variables

---
+++ Variables.java
@@ -0,0 +1,15 @@
+public class Variables{
+	
+	public static void main(String[] args){
+		//declaring three variables and intializing those variables.
+		
+		int maxSpeed = 120;
+		int minutePerHour = 60;
+		String carName = "Volvo";
+		
+		//printing those variables
+		System.out.println("maxSpeed: " + maxSpeed);
+		System.out.println("minutePerHour: " + minutePerHour);
+		System.out.println("carName: " + carName);
+	}
+}
